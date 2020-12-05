f9323387f4e79b373be11b7f9229f8afc9912801
Maxsuel Oliveira Alves <maxsuelo53@gmail.com>
2020-12-04 23:10:56 -0300

    firebase
    adicionar firebase

--- www/js/dados.js
+++ www/js/dados.js
@@ -1,53 +1,70 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDFojLTO-UyT5Jpzh8lFI_GFlMwOSbeZYE",
+    authDomain: "teste-b13da.firebaseapp.com",
+    projectId: "teste-b13da",
+    storageBucket: "teste-b13da.appspot.com",
+    messagingSenderId: "379745143554",
+    appId: "1:379745143554:web:638f92c999a700ab8bd01b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+var tarefasDB = firebase.database().ref("tarefas");
+
+
 var tarefas = [];
 
-function nextID() {
-    var id = 1;
-
-    tarefas.forEach(function(tarefa, index) {
-		if (tarefa.id >= id) id = tarefa.id + 1;
-	});
-	
-	return id;	
-}
 
 function add(){
     
     if ($("#tarefa").val() != ""){
-        var tarefa = {"id":nextID(), "nome": $("#tarefa").val(), "feito":false};
-        tarefas.push(tarefa);
+        var tarefa = {"nome": $("#tarefa").val(), "feito":false};
+        tarefasDB.push(tarefa);
         $("#tarefa").val("");
         list();
     }
 }
 
 function list(){
-    var html = "";
 
-    tarefas.forEach(function(tarefa,index){
-        html += createHTML(tarefa)
+    tarefasDB.once("value",function(tarefas){
+
+
+        var html = "";
+
+        tarefas.forEach(function(tarefa,index){
+            html += createHTML(tarefa.key,tarefa.val());
+        });
+
+        $("#tarefas").html(html);
     });
-
-    $("#tarefas").html(html);
 }
 
-function createHTML(tarefa){
+function createHTML(id,tarefa){
 
     var html ="";
 
     if(tarefa.feito){
-        html += ("<li class='collection-item feito'>"+ "<div onclick='changeStatus(" + tarefa.id + ")'>" + tarefa.nome + " " + tarefa.id + "  "+"<i class='material-icons iconFeito'>done_outline</i> " + "<a class='secondary-content' onclick='del(" + tarefa.id + ")'><i class='material-icons excluir'>delete</i></a></div></li> " );
+        html += ("<li class='collection-item feito'>"+ "<div onclick='changeStatus(\"" + id + "\",this);'>" + tarefa.nome +"<i class='material-icons iconFeito'>done_outline</i> " + "<a class='secondary-content' onclick='del(event,\''+id+'\');return false;'><i class='material-icons excluir'>delete</i></a></div></li> " );
     }else{
-        html += ("<li class='collection-item'>" + "<div onclick='changeStatus(" + tarefa.id + ")'>" + tarefa.nome + " " + tarefa.id + "<a class='secondary-content' onclick='del(" + tarefa.id + ")'><i class='material-icons excluir'>delete</i></a></div></li> " );    
+        html += ("<li class='collection-item'>" + "<div onclick='changeStatus(\"" + id + "\",this);'>" + tarefa.nome + "<a class='secondary-content' onclick='del(event,\"" +id+ "\");return false;'><i class='material-icons excluir'>delete</i></a></div></li> " );    
     }
 
     return html;
 }
 
-function changeStatus(id) {
-	
-	tarefas.forEach(function(tarefa, index) {
-		if (tarefa.id == id) tarefa.feito = !tarefa.feito;
-	});
+function changeStatus(id, obj) {
+
+    var feito = $(obj).parent("li").hasClass("feito");
+    
+    tarefasDB.child(id).update({
+        "feito": !feito
+    })
+    
+	//tarefas.forEach(function(tarefa, index) {
+	//	if (tarefa.id == id) tarefa.feito = !tarefa.feito;
+	//});
 	
 	
 	list();
@@ -56,10 +73,9 @@ function changeStatus(id) {
 
 
 function del(id) {
-		
-	tarefas.forEach(function(tarefa, index) {
-		if (tarefa.id == id) tarefas.splice(index, 1);
-	});
+    
+    evt.stopPropagation();
+	tarefasDB.child(id).remove();
 	
 	
 	list();
